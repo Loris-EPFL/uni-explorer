5caaecda679f4e32f9ccaa3d3bd58c2ac5ad641d
U <nils.del@protonmail.com>
2023-07-22 20:47:15 +0200

    feat: price feed uses start and stop timestamps of position

--- src/components/query-component.tsx
+++ src/components/query-component.tsx
@@ -11,22 +11,80 @@ const chartTest : DataItem[] = [
   { date: '2024', principal: 2000, fees: 33333},
 ];
 
-const GET_OWNER = gql`
-  query GetOwner($id: String!) {
+
+// TODO: Check if transaction is not the latest transaction made to the NFT (does transfer, close, or top up make it change timestamp?)
+
+const GET_TiMESTAMPS = gql`
+  query GetTimestamps($id: String!) {
+    _meta {
+      block {
+        timestamp
+      }
+    }
     position(id: $id) {
-      owner
+      transaction {
+        timestamp
+      }
     }
   }
 `;
+
+const GET_POSITION_DATA = gql`
+  query GetPositionData($id: String!, $date_lte: Int!, $date_gte: Int!) {
+    _meta {
+      block {
+        timestamp
+      }
+    }
+    position(id: $id) {
+      transaction {
+        timestamp
+      }
+      token0 {
+        symbol
+        tokenDayData(where: {date_lte: $date_lte, date_gte: $date_gte}) {
+          date
+          priceUSD
+        }
+      }
+    }
+  }
+`;
+
 
 interface QueryComponentProps {
   id: string;
 }
 
 export default function QueryComponent({ id }: QueryComponentProps) {
-  const { loading, error, data } = useQuery(GET_OWNER, {
-    variables: { id },
+  const { loading, error, data } = useQuery(GET_TiMESTAMPS, {
+    variables: { id: id },
   });
+  if (loading) return <Typography>Loading Timestamps...</Typography>;
+  if (error) return <Typography>Error! {error.message}</Typography>;
+
+  console.log(data)
+
+
+  return(
+    <Box>
+      <Typography>days active : {(data._meta.block.timestamp - parseInt(data.position.transaction.timestamp)) / 3600 / 24}</Typography>
+      <PriceFeed id={id} date_lte={data._meta.block.timestamp} date_gte={parseInt(data.position.transaction.timestamp)}></PriceFeed>
+    </Box>
+  );
+}
+
+interface PriceFeedProps {
+  id: string;
+  date_lte: number;
+  date_gte: number;
+}
+
+function PriceFeed({ id, date_lte, date_gte}: PriceFeedProps) {
+  const { loading, error, data } = useQuery(GET_POSITION_DATA, {
+    variables: { id: id, date_lte: date_lte, date_gte: date_gte },
+  });
+  
 
   if (loading) return <Typography>Loading...</Typography>;
   if (error) return <Typography>Error! {error.message}</Typography>;
